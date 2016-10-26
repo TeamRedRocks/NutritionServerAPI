c11d020b244b22cdcdd498b39b1f4477f284779a
tyler-johnson-13 <tyler.johnson.13@ndsu.edu>
2016-10-26 11:39:15 -0500

    separated config values into new file 'dbcfg.rb'

--- nutritionapp.rb
+++ nutritionapp.rb
@@ -3,10 +3,13 @@ require 'sinatra'
 require 'json'
 require 'mysql2'
 
+require './dbcfg.rb'
+
 set :bind, '0.0.0.0'
+set :port, '4568'
 set :json_content_type, :js
 
-client = Mysql2::Client.new(:username => "app.nutrition", :password => "subXzbKLc*j[t+vz,1$'^Fr_VU@C", :database => "nutrition")
+client = Mysql2::Client.new(:username => $username, :password => $password, :database => $schema, :reconnect => true)
 
 
 get '/' do
@@ -18,6 +21,26 @@ get '/venues' do
     prereturn = { :count => dbResults.count, :venues => [] }
     dbResults.each do |row|
         prereturn[:venues].push({:id => row["ID"], :name => row["Name"]})
+    end
+    prereturn.to_json
+end
+
+get '/venues/:vid/meals' do
+    clean_vid = client.escape(params[:vid]);
+    dbResults = client.query("SELECT * FROM Meals WHERE VenueID='#{clean_vid}'");
+    prereturn = { :count => dbResults.count, :meals => [] }
+    dbResults.each do |row|
+        prereturn[:meals].push({:id => row["ID"], :name => row["Name"], :servingsizeoz => row["ServingSizeOz"], :nutritionvalues => JSON.parse(row["NutritionValues"])})
+    end
+    prereturn.to_json
+end
+
+get '/meals/:mid' do
+    clean_mid = client.escape(params[:mid]);
+    dbResults = client.query("SELECT * FROM Meals WHERE ID='#{clean_mid}'");
+    prereturn = { :count => dbResults.count, :meals => [] }
+    dbResults.each do |row|
+        prereturn[:meals].push({:id => row["ID"], :name => row["Name"], :servingsizeoz => row["ServingSizeOz"], :nutritionvalues => JSON.parse(row["NutritionValues"])})
     end
     prereturn.to_json
 end
